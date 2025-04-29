0ad97fa317fd32a0aea30579c3adbae68c465fca
cklamann <12862284+cklamann@users.noreply.github.com>
2025-04-29 14:35:15 -0400

    add base path

--- next/region-scan-dashboard/next.config.ts
+++ next/region-scan-dashboard/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
