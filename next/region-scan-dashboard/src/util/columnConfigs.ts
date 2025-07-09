fb97de5c889d85bf78340f37f1ceecb124f28988
cklamann <12862284+cklamann@users.noreply.github.com>
2025-07-09 11:15:42 -0400

    use scientific notation for pvalues and round floats

--- next/region-scan-dashboard/src/util/columnConfigs.ts
+++ next/region-scan-dashboard/src/util/columnConfigs.ts
@@ -1,6 +1,11 @@
 import { GridComparatorFn, GridSortDirection } from "@mui/x-data-grid";
+import { format } from "d3-format";
 import { PaginatedTableColumn } from "@/components/PaginatedTable";
 import { RegionResult } from "@/lib/ts/types";
+
+const formatPval = format(".3e");
+
+const formatFloat = format(".3f");
 
 const chromSort =
   (direction: GridSortDirection): GridComparatorFn<number> =>
@@ -53,24 +58,79 @@ export const RegionResultCols: PaginatedTableColumn<RegionResult>[] = [
   },
   { field: "nSNPs", headerName: "nSNPs", sortable: true },
   { field: "nSNPs_kept", headerName: "nSNPs_kept", sortable: true },
-  { field: "maxVIF", headerName: "maxVIF", sortable: true },
+  {
+    field: "maxVIF",
+    headerName: "maxVIF",
+    sortable: true,
+    valueFormatter: formatFloat,
+  },
   { field: "Wald", hideOnLoad: true, headerName: "Wald", sortable: true },
   { field: "Wald_df", headerName: "Wald_df", sortable: true },
-  { field: "Wald_p", headerName: "Wald_p", sortable: true },
+  {
+    field: "Wald_p",
+    headerName: "Wald_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
   { field: "PC80", hideOnLoad: true, headerName: "PC80", sortable: true },
   { field: "PC80_df", headerName: "PC80_df", sortable: true },
-  { field: "PC80_p", headerName: "PC80_p", sortable: true },
+  {
+    field: "PC80_p",
+    headerName: "PC80_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
   { field: "MLCB", hideOnLoad: true, headerName: "MLCB", sortable: true },
   { field: "MLCB_df", headerName: "MLCB_df", sortable: true },
-  { field: "MLCB_p", headerName: "MLCB_p", sortable: true },
+  {
+    field: "MLCB_p",
+    headerName: "MLCB_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
   { field: "LCB", hideOnLoad: true, headerName: "LCB", sortable: true },
   { field: "LCB_df", headerName: "LCB_df", sortable: true },
-  { field: "LCB_p", headerName: "LCB_p", sortable: true },
-  { field: "SKAT_p", headerName: "SKAT_p", sortable: true },
-  { field: "SKATO_p", headerName: "SKATO_p", sortable: true },
-  { field: "simes_p", headerName: "simes_p", sortable: true },
+  {
+    field: "LCB_p",
+    headerName: "LCB_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
+  {
+    field: "SKAT_p",
+    headerName: "SKAT_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
+  {
+    field: "SKATO_p",
+    headerName: "SKATO_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
+  {
+    field: "simes_p",
+    headerName: "simes_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
   { field: "simpleM_df", headerName: "simpleM_df", sortable: true },
-  { field: "simpleM_p", headerName: "simpleM_p", sortable: true },
-  { field: "GATES_p", headerName: "GATES_p", sortable: true },
-  { field: "single_Wald_p", headerName: "single_Wald_p", sortable: true },
+  {
+    field: "simpleM_p",
+    headerName: "simpleM_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
+  {
+    field: "GATES_p",
+    headerName: "GATES_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
+  {
+    field: "single_Wald_p",
+    headerName: "single_Wald_p",
+    sortable: true,
+    valueFormatter: formatPval,
+  },
 ];
